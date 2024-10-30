cef700369dc2412f0d66ef05e6da3a7345074de6
PatelDev14 <141462337+PatelDev14@users.noreply.github.com>
2024-10-30 18:27:38 -0400

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://pateldev14.github.io/weather-app/',
+  base: '/weather-app/',
   plugins: [react()],
 })
 
